218e577b709a1fdf4d52132bb21d860190d4315b
JyothikSandeep <101253647+JyothikSandeep@users.noreply.github.com>
2024-12-18 01:33:52 +0530

    added env file

--- mongoDb/mongoConnect.js
+++ mongoDb/mongoConnect.js
@@ -1,14 +1,21 @@
-const mongoose=require('mongoose');
+require("dotenv").config(); // Load environment variables
+const mongoose = require("mongoose");
 
-const connectDb=async()=>{
+const connectDb = async () => {
+ 
+  const uri = process.env.MONGO_URI;
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB Atlas successfully!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB Atlas:", error.message);
+  }
+  // await mongoose.connect("mongodb://localhost:27017/testing").then(()=>{
+  //     console.log("connected to database")
+  // }).catch((err)=>{
+  //     console.log(err.message);
 
-    await mongoose.connect("mongodb://localhost:27017/testing").then(()=>{
-        console.log("connected to database")
-    }).catch((err)=>{
-        console.log(err.message);
+  // });
+};
 
-    });
-    
-}
-
-module.exports={connectDb}
+module.exports = { connectDb };
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require('dotenv').config(); // Load environment variables
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 const { connectDb } = require("./mongoDb/mongoConnect");
 const { loginrouter } = require("./Routes/LoginRoute");
 const { adminRouter } = require("./Routes/AdminRoutes");
@@ -10,7 +12,7 @@ app.use(express.json());
 app.use("/", loginrouter);
 app.use("/admin", adminRouter); 
 app.use("/user",userRouter)
-app.listen(5000, () => {
+app.listen(process.env.PORT, () => {
   connectDb();
   console.log("listening to server");
 });
